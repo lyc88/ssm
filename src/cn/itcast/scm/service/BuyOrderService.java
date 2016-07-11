de0997920f0557318bb93c1f6939895d0f6db318
lyc <984006207@qq.com>
2016-07-11 23:42:29 +0800

    ssm项目

---
+++ src/cn/itcast/scm/service/BuyOrderService.java
@@ -0,0 +1,9 @@
+package cn.itcast.scm.service;
+
+import cn.itcast.scm.entity.BuyOrder;
+
+public interface BuyOrderService extends BaseService<BuyOrder> {
+	
+	public int insertBuyOrder(BuyOrder buyOrder) throws Exception;
+	 
+}
